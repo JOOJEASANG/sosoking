d70e543bd4d0c4dfb5822e2a8eb9aa547c7f271b
주재상 <joojeasang@gmail.com>
2026-05-13 03:00:23 +0900

    Keep legacy AI hunt functions exported to avoid delete failure

--- functions/functions-main-v2.js
+++ functions/functions-main-v2.js
@@ -1,7 +1,9 @@
 const existingFunctions = require('./functions-main.js');
 const predictionFunctions = require('./prediction-functions.js');
+const aiHuntFunctions = require('./ai-hunt-functions.js');
 
 module.exports = {
   ...existingFunctions,
+  ...aiHuntFunctions,
   ...predictionFunctions,
 };
