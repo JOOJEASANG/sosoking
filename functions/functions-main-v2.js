a817446ca616b3d294a9fa02bbc1c7bb06458263
주재상 <joojeasang@gmail.com>
2026-05-17 03:32:41 +0900

    Remove legacy prediction board exports

--- functions/functions-main-v2.js
+++ functions/functions-main-v2.js
@@ -4,8 +4,6 @@ module.exports = {
   ...require('./sitemap-functions.js'),
   ...require('./ai-mission-functions.js'),
   ...require('./ai-hunt-functions.js'),
-  ...require('./prediction-functions.js'),
-  ...require('./hot-issue-functions.js'),
   ...require('./settlement-functions.js'),
   ...require('./link-summary-functions.js'),
   ...require('./daily-seed-functions.js'),
