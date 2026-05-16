0377fc0f85bcad5768af4373aae5632762741cd5
주재상 <joojeasang@gmail.com>
2026-05-16 21:19:51 +0900

    Wire secure account actions

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ import { renderBottomNav } from './components/bottom-nav.js';
 import { initToast } from './components/toast.js';
 import { appState } from './state.js';
 import './secure-feed-actions.js';
+import './account-secure-actions.js';
 import './social-play-enhancer.js';
 import {
   collection, query, where, getDocs, getDoc, doc, limit,
