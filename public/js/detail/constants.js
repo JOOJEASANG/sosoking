a6937275340f3770a6cc5191eab2fe0e70b37624
주재상 <joojeasang@gmail.com>
2026-05-20 15:15:01 +0900

    Add detail constants module

---
+++ public/js/detail/constants.js
@@ -0,0 +1,40 @@
+export const TYPE_LABELS = {
+  balance: '밸런스게임',
+  vote: '투표',
+  battle: '선택지배틀',
+  challenge24: '24시간챌린지',
+  tournament: '이상형월드컵',
+  naming: '미친작명소',
+  acrostic: '삼행시짓기',
+  drip: '한줄드립',
+  cbattle: '댓글배틀',
+  laugh: '웃참챌린지',
+  ox: 'OX퀴즈',
+  quiz: '4지선다',
+  relay: '막장릴레이',
+  word_relay: '단어릴레이',
+  random_battle: '랜덤대결',
+  howto: '노하우',
+  story: '경험담',
+  fail: '실패담',
+  concern: '고민/질문',
+  multi: '피드 글',
+};
+
+export const CAT_CLASS = {
+  golra: 'golra',
+  usgyo: 'usgyo',
+  malhe: 'malhe',
+};
+
+export const ACROSTIC_REACTIONS = [
+  { key: 'like', emoji: '👍' },
+  { key: 'funny', emoji: '😂' },
+  { key: 'fire', emoji: '🔥' },
+];
+
+export const COMMENT_REACTIONS = [
+  { key: 'funny', emoji: '😂' },
+  { key: 'fire', emoji: '🔥' },
+  { key: 'like', emoji: '👍' },
+];
