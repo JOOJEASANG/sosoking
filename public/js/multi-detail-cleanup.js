fe5a53995df2501982afd612d5ebac5b9f3ccda3
주재상 <joojeasang@gmail.com>
2026-05-21 19:59:10 +0900

    Remove disabled legacy multi detail cleanup

--- public/js/multi-detail-cleanup.js
+++
@@ -1,7 +0,0 @@
-// multi-detail-cleanup.js
-//
-// 최신 참여 기능 렌더링은 public/js/multi-detail.js와 public/js/multi-detail/render.js가 담당합니다.
-// 이 레거시 cleanup 파일은 렌더링된 참여 모듈을 다시 제거하거나 제목을 덮어써
-// 여러 기능이 들어간 글에서 UI가 사라질 수 있어 비활성화합니다.
-
-console.info('[multi-detail-cleanup] disabled: multi-detail.js owns participation UI');
