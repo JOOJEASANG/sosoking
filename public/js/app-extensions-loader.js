d4eb1216dabafb72bda118d9af9c6d797e0651a4
주재상 <joojeasang@gmail.com>
2026-05-22 09:43:54 +0900

    Load uid based account notification fix

--- public/js/app-extensions-loader.js
+++ public/js/app-extensions-loader.js
@@ -47,6 +47,7 @@ const EXTENSION_MODULES = [
   './admin-ui-cleanup.js',
   './notifications-panel.js',
   './account-notifications-section.js',
+  './account-notifications-uid-fix.js',
 
   // 추가 표시 보강
   './relay-mission-cards.js',
