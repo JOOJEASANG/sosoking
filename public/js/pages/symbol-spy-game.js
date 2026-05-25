a0e3da96f65829f064234c4e7a75a1e87c15c5f6
주재상 <joojeasang@gmail.com>
2026-05-26 00:23:08 +0900

    Load Symbol Spy styles from page entry

--- public/js/pages/symbol-spy-game.js
+++ public/js/pages/symbol-spy-game.js
@@ -1,1 +1,11 @@
+function ensureSymbolSpyStyles() {
+  if (document.querySelector('link[href="/css/symbol-spy-game.css"]')) return;
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = '/css/symbol-spy-game.css';
+  document.head.appendChild(link);
+}
+
+ensureSymbolSpyStyles();
+
 export { renderSymbolSpyGame } from '../games/symbol-spy/index.js';
