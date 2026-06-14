c28a439edcc24f9c5d27c499810c029816cd504a
주재상 <joojeasang@gmail.com>
2026-06-14 13:47:33 +0900

    상세 화면 시민발언 타입 라벨 추가

--- public/js/detail/constants.js
+++ public/js/detail/constants.js
@@ -1,4 +1,5 @@
 export const TYPE_LABELS = {
+  citizen_speech: '시민발언',
   tournament: '대결방',
   vote: '토론방',
   crazy_court: '토론방',
@@ -8,7 +9,7 @@ export const TYPE_LABELS = {
   cbattle: '드립방',
   multi: '일반방',
   general: '일반방',
-  ai_judge: 'AI판사',
+  ai_judge: '헌재기록',
 };
 
 export const CAT_CLASS = {
@@ -16,6 +17,8 @@ export const CAT_CLASS = {
   usgyo: 'usgyo',
   malhe: 'malhe',
   multi: 'multi',
+  citizen_speech: 'multi',
+  ai_judge: 'golra',
 };
 
 export const COMMENT_REACTIONS = [
