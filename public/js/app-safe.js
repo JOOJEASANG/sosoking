bf88112ea66ea6e4ddf113018219b112cb0efc3a
주재상 <joojeasang@gmail.com>
2026-06-14 13:43:36 +0900

    시민광장 글쓰기 라우트 등록

--- public/js/app-safe.js
+++ public/js/app-safe.js
@@ -108,7 +108,8 @@ async function registerRoutes() {
   registerRoute('/election', async () => renderPage((await import('./pages/election.js')).renderElection, '대통령 선거'));
   registerRoute('/ranking', async () => renderPage((await import('./pages/ranking.js')).renderRanking, '정치력 랭킹'));
   registerRoute('/king-history', async () => renderPage((await import('./pages/king-history.js')).renderKingHistory, '역대 당선자'));
-  registerRoute('/feed', async () => renderPage((await import('./pages/feed.js')).renderFeed, '피드'));
+  registerRoute('/feed', async () => renderPage((await import('./pages/feed.js')).renderFeed, '시민광장'));
+  registerRoute('/write', async () => renderPage((await import('./pages/write.js')).renderWrite, '시민발언 작성'));
   registerRoute('/hall', async () => renderPage((await import('./pages/hall.js')).renderHall, '통계'));
   registerRoute('/account', async () => renderPage(renderAccountSafe, '내 정보'));
   registerRoute('/scraps', async () => renderPage((await import('./pages/scraps.js')).renderScraps, '스크랩'));
@@ -205,7 +206,7 @@ function renderFrame() {
             <div class="site-footer__inner">
               <div class="site-footer__brand-block"><a href="#/" class="site-footer__brand"><img src="/logo.svg" alt="" width="26" height="26"><span>소소킹</span></a><div class="site-footer__tagline">3개 정당 AI 정치인의 가상 정치 공화국<br>매일 새로운 당선자가 탄생합니다</div></div>
               <div><div class="site-footer__col-title">공화국</div><div class="site-footer__links"><a href="#/republic">🏛️ 공화국 허브</a><a href="#/battle">⚔️ 정치배틀</a><a href="#/election">👑 대선</a><a href="#/congress">🏛️ 국회</a><a href="#/constitutional-court">⚖️ 헌법재판소</a><a href="#/news">📰 소소신문</a><a href="#/king-history">🏆 역대 당선자</a></div></div>
-              <div><div class="site-footer__col-title">커뮤니티</div><div class="site-footer__links"><a href="#/feed">피드</a><a href="#/ranking">랭킹</a><a href="#/parties">정당 상세</a><a href="#/guide">이용안내</a></div></div>
+              <div><div class="site-footer__col-title">커뮤니티</div><div class="site-footer__links"><a href="#/feed">시민광장</a><a href="#/ranking">랭킹</a><a href="#/parties">정당 상세</a><a href="#/guide">이용안내</a></div></div>
               <div><div class="site-footer__col-title">정보</div><div class="site-footer__links"><a href="#/terms">이용약관</a><a href="#/privacy">개인정보처리방침</a></div></div>
             </div>
           </div>
