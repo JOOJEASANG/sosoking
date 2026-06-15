11191d02a7924666f07640e744491224b69f960c
주재상 <joojeasang@gmail.com>
2026-06-15 20:15:56 +0900

    Keep detail constants political only

--- public/js/detail/constants.js
+++ public/js/detail/constants.js
@@ -1,21 +1,9 @@
 export const TYPE_LABELS = {
   citizen_speech: '시민발언',
-  tournament: '대결방',
-  vote: '토론방',
-  crazy_court: '토론방',
-  battle: '토론방',
-  balance: '토론방',
-  drip: '드립방',
-  cbattle: '드립방',
-  multi: '일반방',
-  general: '일반방',
   ai_judge: '헌재기록',
 };
 
 export const CAT_CLASS = {
-  golra: 'golra',
-  usgyo: 'usgyo',
-  malhe: 'malhe',
   multi: 'multi',
   citizen_speech: 'multi',
   ai_judge: 'golra',
