f364501c3da0678b6f0db4e60967a2760cd6d252
주재상 <joojeasang@gmail.com>
2026-06-17 17:14:38 +0900

    Restore congress page as redirect shim

---
+++ public/js/pages/congress.js
@@ -0,0 +1,4 @@
+import { navigate } from '../router.js';
+export function renderCongress() { navigate('/materials'); }
+export function renderCongressPage() { navigate('/materials'); }
+export default renderCongress;
