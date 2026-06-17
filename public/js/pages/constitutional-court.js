2f0c1ae96342decdc1102be505a2e89e201a1e95
주재상 <joojeasang@gmail.com>
2026-06-17 17:14:49 +0900

    Restore court page as redirect shim

---
+++ public/js/pages/constitutional-court.js
@@ -0,0 +1,4 @@
+import { navigate } from '../router.js';
+export function renderConstitutionalCourt() { navigate('/materials'); }
+export function renderConstitutionalCourtPage() { navigate('/materials'); }
+export default renderConstitutionalCourt;
