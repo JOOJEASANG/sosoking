0a3a42df9f789660478cef48995e91388b937908
주재상 <joojeasang@gmail.com>
2026-06-17 17:14:20 +0900

    Restore election page as redirect shim

---
+++ public/js/pages/election.js
@@ -0,0 +1,4 @@
+import { navigate } from '../router.js';
+export function renderElection() { navigate('/materials'); }
+export function renderElectionPage() { navigate('/materials'); }
+export default renderElection;
