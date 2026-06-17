c61aa4b1d27eda50cd927fa21eae59f278c3e4ad
주재상 <joojeasang@gmail.com>
2026-06-17 17:15:05 +0900

    Restore news page as redirect shim

---
+++ public/js/pages/news.js
@@ -0,0 +1,4 @@
+import { navigate } from '../router.js';
+export function renderNews() { navigate('/materials'); }
+export function renderNewsPage() { navigate('/materials'); }
+export default renderNews;
