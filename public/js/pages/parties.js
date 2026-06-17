5e07905b988a30557c46950d138f5f53fb5b125e
주재상 <joojeasang@gmail.com>
2026-06-17 17:14:53 +0900

    Restore parties page as redirect shim

---
+++ public/js/pages/parties.js
@@ -0,0 +1,4 @@
+import { navigate } from '../router.js';
+export function renderParties() { navigate('/materials'); }
+export function renderPartiesPage() { navigate('/materials'); }
+export default renderParties;
