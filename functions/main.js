a9b24a40722db0f8173dd22b42d9e991f3b3b4ed
주재상 <joojeasang@gmail.com>
2026-07-01 07:23:11 +0900

    Use secure submitCase override

--- functions/main.js
+++ functions/main.js
@@ -2,3 +2,4 @@ Object.assign(exports, require('./index'));
 Object.assign(exports, require('./daily'));
 Object.assign(exports, require('./profile'));
 Object.assign(exports, require('./social'));
+Object.assign(exports, require('./submit-secure'));
