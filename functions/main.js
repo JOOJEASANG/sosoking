a65070d422f861a23f9b12d8a8e839a3f81d8245
주재상 <joojeasang@gmail.com>
2026-07-03 14:31:56 +0900

    Delete functions directory

--- functions/main.js
+++
@@ -1,7 +0,0 @@
-Object.assign(exports, require('./index'));
-Object.assign(exports, require('./daily'));
-Object.assign(exports, require('./profile'));
-Object.assign(exports, require('./social'));
-Object.assign(exports, require('./submit-secure'));
-Object.assign(exports, require('./generate-trial-lite'));
-Object.assign(exports, require('./admin-actions'));
