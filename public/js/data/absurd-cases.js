99b82d608d4422d63b1e3e888f0db4144906a792
주재상 <joojeasang@gmail.com>
2026-07-07 21:11:16 +0900

    Restore empty absurd cases data file

---
+++ public/js/data/absurd-cases.js
@@ -0,0 +1,6 @@
+// 황당사례 모음 데이터 자리입니다.
+// 현재는 사용자가 원치 않은 100개 예시 데이터를 제거하고 빈 배열로 둡니다.
+// 나중에 원하는 사례만 이 배열에 추가하면 /#/absurd-cases 페이지에 노출됩니다.
+
+export const ABSURD_CASES = [];
+export const ABSURD_CASE_CATEGORIES = [];
