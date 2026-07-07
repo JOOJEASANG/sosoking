a83181e4223d23be96753d090f551a6d88a4b812
주재상 <joojeasang@gmail.com>
2026-07-07 23:00:45 +0900

    Load updated home logo asset

--- public/js/pages/home-court.js
+++ public/js/pages/home-court.js
@@ -1,4 +1,4 @@
-import { renderHome as renderBaseHome } from './home.js?v=20260707-2';
+import { renderHome as renderBaseHome } from './home.js?v=20260707-logo2';
 
 function addCourtEntrance(container) {
   const hero = container.querySelector('.hero-section');
