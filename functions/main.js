f967d649573a3608a7ec9b26a1f7a042105b305a
주재상 <joojeasang@gmail.com>
2026-07-08 22:52:41 +0900

    Export AI title suggestion function

--- functions/main.js
+++ functions/main.js
@@ -8,6 +8,7 @@ Object.assign(exports, require('./daily'));
 Object.assign(exports, require('./profile'));
 Object.assign(exports, require('./social'));
 Object.assign(exports, require('./submit-secure'));
+Object.assign(exports, require('./title-suggestion'));
 Object.assign(exports, require('./generate-trial-lite'));
 Object.assign(exports, require('./admin-actions'));
 Object.assign(exports, require('./repair'));
