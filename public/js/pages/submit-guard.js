5c229c78f294a60bb99baa1c43f27f4190eb325b
주재상 <joojeasang@gmail.com>
2026-07-08 16:31:44 +0900

    Bump submit court page version

--- public/js/pages/submit-guard.js
+++ public/js/pages/submit-guard.js
@@ -1,5 +1,5 @@
 import { auth } from '../firebase.js?v=20260630-3';
-import { renderSubmit as renderSubmitForm } from './submit-court.js?v=20260707-5';
+import { renderSubmit as renderSubmitForm } from './submit-court.js?v=20260708-submit1';
 
 function loginRequired(container) {
   container.innerHTML = `
