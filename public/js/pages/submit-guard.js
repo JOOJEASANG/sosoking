4975ed3ea063f06418bde01c9d0091e5ae25f79e
주재상 <joojeasang@gmail.com>
2026-07-08 20:31:53 +0900

    Bump submit court smart title cache

--- public/js/pages/submit-guard.js
+++ public/js/pages/submit-guard.js
@@ -1,5 +1,5 @@
 import { auth } from '../firebase.js?v=20260630-3';
-import { renderSubmit as renderSubmitForm } from './submit-court.js?v=20260708-simple1';
+import { renderSubmit as renderSubmitForm } from './submit-court.js?v=20260708-title1';
 
 function loginRequired(container) {
   container.innerHTML = `
