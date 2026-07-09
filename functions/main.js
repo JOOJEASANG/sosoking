18ac5222ef36aea256c08750bcdfcad880caca74
주재상 <joojeasang@gmail.com>
2026-07-09 12:44:40 +0900

    Remove legacy security patch override from functions entrypoint

--- functions/main.js
+++ functions/main.js
@@ -12,7 +12,3 @@ Object.assign(exports, require('./title-suggestion'));
 Object.assign(exports, require('./generate-trial-lite'));
 Object.assign(exports, require('./admin-actions'));
 Object.assign(exports, require('./repair'));
-
-// 보안·운영 안정성 보강판을 마지막에 로드해 기존 동일 함수명을 덮어쓴다.
-// 기존 파일은 보존하되 배포 시 아래 구현이 실제 callable function으로 노출된다.
-Object.assign(exports, require('./security-patches'));
