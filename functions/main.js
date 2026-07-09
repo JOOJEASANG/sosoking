649d51419f4dc57a8c32c327827c758fa1cd0698
주재상 <joojeasang@gmail.com>
2026-07-09 09:39:10 +0900

    Use safe fresh generator module

--- functions/main.js
+++ functions/main.js
@@ -6,5 +6,5 @@ if (!getApps().length) {
 
 Object.assign(exports, require('./fresh-submit'));
 Object.assign(exports, require('./fresh-title'));
-Object.assign(exports, require('./fresh-generate'));
+Object.assign(exports, require('./fresh-generate-safe'));
 Object.assign(exports, require('./fresh-social'));
