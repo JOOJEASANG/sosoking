eac4c458b09493109564a0cf5ecd625687d3a55e
주재상 <joojeasang@gmail.com>
2026-07-09 09:22:27 +0900

    Reset functions entry to fresh app modules

--- functions/main.js
+++ functions/main.js
@@ -4,11 +4,7 @@ if (!getApps().length) {
   initializeApp();
 }
 
-Object.assign(exports, require('./daily'));
-Object.assign(exports, require('./profile'));
-Object.assign(exports, require('./social'));
-Object.assign(exports, require('./submit-secure'));
-Object.assign(exports, require('./title-suggestion'));
-Object.assign(exports, require('./generate-trial-lite'));
-Object.assign(exports, require('./admin-actions'));
-Object.assign(exports, require('./repair'));
+Object.assign(exports, require('./fresh-submit'));
+Object.assign(exports, require('./fresh-title'));
+Object.assign(exports, require('./fresh-generate'));
+Object.assign(exports, require('./fresh-social'));
