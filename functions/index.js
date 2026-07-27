98b5db98904a4e59bf48618d3799f2a0aa9d1224
주재상 <joojeasang@gmail.com>
2026-07-27 14:12:06 +0900

    Delete functions directory

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const investigation = require("./gemini-court-investigation");
-const automaticStory = require("./gemini-auto-case-story");
-
-exports.generateCourtCase = investigation.generateCourtCase;
-exports.generateCourtCaseV3 = investigation.generateCourtCase;
-exports.generateCourtCaseV6 = automaticStory.generateCourtCaseV6;
