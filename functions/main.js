dffd9ebf6d2aec1d7c49f2b07a55d8ade170aa4a
주재상 <joojeasang@gmail.com>
2026-07-29 05:44:39 +0900

    Export secure report submission

--- functions/main.js
+++ functions/main.js
@@ -7,6 +7,7 @@ if (!getApps().length) initializeApp();
 Object.assign(exports, require('./daily'));
 Object.assign(exports, require('./profile'));
 Object.assign(exports, require('./social'));
+Object.assign(exports, require('./reports'));
 Object.assign(exports, require('./submit-secure'));
 Object.assign(exports, require('./generate-trial-lite'));
 Object.assign(exports, require('./admin-actions'));
