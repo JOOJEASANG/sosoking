5ea0be002e43e0aa9fbd43e1898c46f82bbe915b
주재상 <joojeasang@gmail.com>
2026-07-29 08:34:06 +0900

    Bump CSP-safe board module version

--- public/js/pages/board-court.js
+++ public/js/pages/board-court.js
@@ -1,4 +1,4 @@
-import { renderBoard as renderBaseBoard } from './board.js?v=20260729-logo-feed-1';
+import { renderBoard as renderBaseBoard } from './board.js?v=20260729-script-csp-1';
 
 function ensureBoardGameStyle() {
   if (document.getElementById('board-game-style')) return;
