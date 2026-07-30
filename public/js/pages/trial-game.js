c0902383c9e7ba871d15ca4c03047b543290e41a
주재상 <joojeasang@gmail.com>
2026-07-30 17:12:59 +0900

    Apply processing page redesign host

--- public/js/pages/trial-game.js
+++ public/js/pages/trial-game.js
@@ -1,5 +1,6 @@
 import { renderTrial as renderBaseTrial } from './trial.js?v=20260729-dark-record-participation-1';
 
 export async function renderTrial(container, caseId) {
+  container.classList.add('trial-redesign-host');
   await renderBaseTrial(container, caseId);
 }
