7eb68ee0716152c6014c72f8c39592f15e06fa1c
주재상 <joojeasang@gmail.com>
2026-07-30 10:37:34 +0900

    Refresh configurable submission form module

--- public/js/pages/submit-guard.js
+++ public/js/pages/submit-guard.js
@@ -1,5 +1,5 @@
 import { auth } from '../firebase.js?v=20260728-audit-1';
-import { renderSubmit as renderSubmitForm } from './submit-court.js?v=20260728-audit-1';
+import { renderSubmit as renderSubmitForm } from './submit-court.js?v=20260730-configurable-limit-1';
 
 function loginRequired(container) {
   container.innerHTML = `
