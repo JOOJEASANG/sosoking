a43c063ffd65b6ca1ed707a5a0e656deeea657bf
주재상 <joojeasang@gmail.com>
2026-07-31 19:07:04 +0900

    판결문 완결성 패치 로드

--- functions/main.js
+++ functions/main.js
@@ -6,6 +6,8 @@ if (!getApps().length) initializeApp();
 
 // Gemini 요청에 소소킹 전용 코미디 강도 규칙을 먼저 주입한다.
 require('./humor-prompt');
+// 번호 항목 분리와 문장 완결성을 보장하는 출력 규칙을 이어서 주입한다.
+require('./document-output-quality');
 
 Object.assign(exports, require('./daily'));
 Object.assign(exports, require('./daily-real-court'));
