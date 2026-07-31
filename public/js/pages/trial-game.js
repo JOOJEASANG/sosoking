6599d08f2c33d6cf5dd78088061159ec78678a07
주재상 <joojeasang@gmail.com>
2026-07-31 17:40:02 +0900

    재판 진행 화면 캐시 버전 갱신

--- public/js/pages/trial-game.js
+++ public/js/pages/trial-game.js
@@ -1,4 +1,4 @@
-import { renderTrial as renderBaseTrial } from './trial.js?v=20260729-dark-record-participation-1';
+import { renderTrial as renderBaseTrial } from './trial.js?v=20260731-judge-trial-progress-1';
 
 export async function renderTrial(container, caseId) {
   await renderBaseTrial(container, caseId);
